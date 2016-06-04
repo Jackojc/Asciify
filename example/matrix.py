4dddc1879a7d57c2b5a18d91766b15e8c35eeadc
Jackojc <jackojc@gmail.com>
2016-06-04 03:10:09 +0100

    fixed directory
    file was in the wrong directory, whoops

--- example/matrix.py
+++ example/matrix.py
@@ -4,24 +4,39 @@ import time
 import pygame
 
 width, height = (60, 30)
-render = asciify(width, height, "font", fontsize=16)
-
 add_per_frame = 5
 
+render = asciify(width, height, "font", fontsize=16)
 random.seed(time.time())
-Particles = [[random.randint(0, width), random.randint(-1, height), random.uniform(0.1, 1.5)] for x in range(add_per_frame)]
 
+Particles = [
+                [
+                    random.randint(0, width),
+                    random.randint(-1, height),
+                    random.uniform(0.1, 1.5)
+                ] for x in range(add_per_frame)
+            ]
 chars = list("abcdefghijklmnopqrstuvwxyz0123456789!\"$%^&*()_+-=[]{}:;@'~#|\<>?,./")
 
 while True:
     render.checkexit()
     keys = render.listkeys()
+
     for num, part in enumerate(Particles):
         part[1] += part[2]
         if part[1] > height+1:
             del Particles[num]
+
         colorrand = (random.randint(60, 255), random.randint(150, 255), random.randint(1, 100))
         render.setString(part[0], part[1], random.choice(chars), color=colorrand)
-    Particles.extend([[random.randint(0, width), random.randint(-1, height), random.uniform(0.1, 1.5)] for x in range(add_per_frame)])
+
+    Particles.extend([
+                        [
+                            random.randint(0, width),
+                            random.randint(-1, height),
+                            random.uniform(0.1, 1.5)
+                        ] for x in range(add_per_frame)
+                    ])
     render.text(0, 0, "THE MATRIX EXAMPLE", center=(1, 1))
+
     render.update(30)
