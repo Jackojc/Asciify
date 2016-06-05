a348e98a941a9882fe92cbec3f13e4a9817aeffd
Jack Clarke <jackojc@gmail.com>
2016-06-05 04:42:46 +0100

    pep8
    more pep

--- asciify.py
+++ asciify.py
@@ -5,17 +5,23 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 
 class asciify():
-    def __init__(self, width, height, font, margin=0, fontsize=16, bgcolor=(0, 0, 0), color=(255, 255, 255), offsets=(0, 0, 0, 0), aa=True):
+
+    def __init__(
+        self, width, height, font, margin=0, fontsize=16, bgcolor=(
+            0, 0, 0), color=(
+            255, 255, 255), offsets=(
+                0, 0, 0, 0), aa=True):
         self.OffsetX, self.OffsetY, self.CellOffsetX, self.CellOffsetY = offsets
         self.Alias = aa
         self.Images = {}
 
         pygame.init()
         if ".ttf" not in font:
-            font = font+".ttf"
+            font = font + ".ttf"
         self.Font = pygame.font.Font(font, fontsize)
 
-        chars = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789£$€%&_+-=@~#<>?")
+        chars = list(
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789£$€%&_+-=@~#<>?")
         maxcharw = 0
         maxcharh = 0
 
@@ -26,7 +32,7 @@ class asciify():
             if yt > maxcharh:
                 maxcharh = yt
 
-        cellx, celly = maxcharw+self.CellOffsetX, maxcharh+self.CellOffsetY
+        cellx, celly = maxcharw + self.CellOffsetX, maxcharh + self.CellOffsetY
 
         self.CellsWidth = width
         self.CellsHeight = height
@@ -44,9 +50,11 @@ class asciify():
         self.CellY = celly
 
         self.Clock = pygame.time.Clock()
-        self.Screen = pygame.display.set_mode((self.Width, self.Height), pygame.DOUBLEBUF | pygame.HWSURFACE)
+        self.Screen = pygame.display.set_mode(
+            (self.Width, self.Height), pygame.DOUBLEBUF | pygame.HWSURFACE)
 
-        self.Foreground = pygame.Surface(self.Screen.get_size(), pygame.SRCALPHA, 32)
+        self.Foreground = pygame.Surface(
+            self.Screen.get_size(), pygame.SRCALPHA, 32)
         self.Foreground = self.Foreground.convert_alpha()
         self.Background = pygame.Surface(self.Screen.get_size())
 
@@ -58,11 +66,13 @@ class asciify():
         posY = (self.CellY * y) + (self.Margin * 2)
 
         if string is None:
-            pygame.draw.rect(self.Background, color, (posX, posY, self.CellX, self.CellY))
+            pygame.draw.rect(self.Background, color,
+                             (posX, posY, self.CellX, self.CellY))
         else:
             string = string[0]
             string = self.Font.render(string, self.Alias, color)
-            self.Foreground.blit(string, (posX+self.OffsetX, posY+self.OffsetY))
+            self.Foreground.blit(
+                string, (posX + self.OffsetX, posY + self.OffsetY))
 
     def setStringBlock(self, x, y, string=None, color=None, bgcolor=None):
         if color is None:
@@ -74,10 +84,12 @@ class asciify():
         posX = (self.CellX * x) + (self.Margin * 2)
         posY = (self.CellY * y) + (self.Margin * 2)
 
-        pygame.draw.rect(self.Background, bgcolor, (posX, posY, self.CellX, self.CellY))
+        pygame.draw.rect(self.Background, bgcolor,
+                         (posX, posY, self.CellX, self.CellY))
         string = string[0]
         string = self.Font.render(string, self.Alias, color)
-        self.Foreground.blit(string, (posX+self.OffsetX, posY+self.OffsetY))
+        self.Foreground.blit(
+            string, (posX + self.OffsetX, posY + self.OffsetY))
 
     def setString(self, x, y, string, color=None):
         if color is None:
@@ -88,7 +100,8 @@ class asciify():
 
         string = string[0]
         string = self.Font.render(string, self.Alias, color)
-        self.Foreground.blit(string, (posX+self.OffsetX, posY+self.OffsetY))
+        self.Foreground.blit(
+            string, (posX + self.OffsetX, posY + self.OffsetY))
 
     def setBlock(self, x, y, color=None):
         if color is None:
@@ -97,7 +110,8 @@ class asciify():
         posX = (self.CellX * x) + (self.Margin * 2)
         posY = (self.CellY * y) + (self.Margin * 2)
 
-        pygame.draw.rect(self.Foreground, color, (posX, posY, self.CellX, self.CellY))
+        pygame.draw.rect(self.Foreground, color,
+                         (posX, posY, self.CellX, self.CellY))
 
     def setBg(self, x, y, color=None):
         if color is None:
@@ -106,13 +120,15 @@ class asciify():
         posX = (self.CellX * x) + (self.Margin * 2)
         posY = (self.CellY * y) + (self.Margin * 2)
 
-        pygame.draw.rect(self.Background, color, (posX, posY, self.CellX, self.CellY))
+        pygame.draw.rect(self.Background, color,
+                         (posX, posY, self.CellX, self.CellY))
 
     def loadImg(self, path):
         image = self.Images.get(path)
 
         if image is None:
-            canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+            canonicalized_path = path.replace(
+                '/', os.sep).replace('\\', os.sep)
             image = pygame.image.load(canonicalized_path)
             self.Images[path] = image
 
@@ -127,7 +143,18 @@ class asciify():
         else:
             self.Background.blit(self.Images[path], (posX, posY))
 
-    def textFixed(self, x, y, string, color=None, shadow=1, shadowcolor=(20, 20, 20), shadowdist=2):
+    def textFixed(
+            self,
+            x,
+            y,
+            string,
+            color=None,
+            shadow=1,
+            shadowcolor=(
+                20,
+                20,
+                20),
+            shadowdist=2):
         if color is None:
             color = self.DefColor
 
@@ -136,12 +163,17 @@ class asciify():
 
         if shadow:
             text = self.Font.render(string, self.Alias, shadowcolor)
-            self.Foreground.blit(text, (posX+self.OffsetX, posY+self.OffsetY+shadowdist))
+            self.Foreground.blit(
+                text, (posX + self.OffsetX, posY + self.OffsetY + shadowdist))
 
         text = self.Font.render(string, self.Alias, color)
-        self.Foreground.blit(text, (posX+self.OffsetX, posY+self.OffsetY))
+        self.Foreground.blit(text, (posX + self.OffsetX, posY + self.OffsetY))
 
-    def text(self, x, y, string, center=(0, 0), padchars=("", ""), transbg=True, color=None, vertical=False, shadow=1, shadowcolor=(20, 20, 20)):
+    def text(
+        self, x, y, string, center=(
+            0, 0), padchars=(
+            "", ""), transbg=True, color=None, vertical=False, shadow=1, shadowcolor=(
+                20, 20, 20)):
         if color is None:
             color = self.DefColor
 
@@ -149,18 +181,18 @@ class asciify():
 
         if center[1]:
             if vertical:
-                y = x + round(((self.CellsHeight/2) - (len(string)/2)))
+                y = x + round(((self.CellsHeight / 2) - (len(string) / 2)))
             else:
-                y = y + round((self.CellsHeight/2))
+                y = y + round((self.CellsHeight / 2))
 
         else:
             y = y
 
         if center[0]:
             if vertical:
-                x = y + round((self.CellsWidth/2))
+                x = y + round((self.CellsWidth / 2))
             else:
-                x = x + round(((self.CellsWidth/2) - (len(string)/2)))
+                x = x + round(((self.CellsWidth / 2) - (len(string) / 2)))
         else:
             x = x
 
@@ -168,29 +200,38 @@ class asciify():
             if transbg:
                 if not vertical:
                     if shadow:
-                        self.setStringBlock(x+num, y+0.07, char, color=shadowcolor)
-                    self.setString(x+num, y, char, color=color)
+                        self.setStringBlock(
+                            x + num, y + 0.07, char, color=shadowcolor)
+                    self.setString(x + num, y, char, color=color)
                 else:
                     if shadow:
-                        self.setStringBlock(x+num, y+0.07, char, color=shadowcolor)
-                    self.setString(x, y+num, char, color=color)
+                        self.setStringBlock(
+                            x + num, y + 0.07, char, color=shadowcolor)
+                    self.setString(x, y + num, char, color=color)
 
             else:
                 if not vertical:
                     if shadow:
-                        self.setStringBlock(x+num,  y+0.07, char, color=shadowcolor, bgcolor=self.DefBgColor)
-                    self.setStringBlock(x+num, y, char, color=color, bgcolor=self.DefBgColor)
+                        self.setStringBlock(
+                            x + num,
+                            y + 0.07,
+                            char,
+                            color=shadowcolor,
+                            bgcolor=self.DefBgColor)
+                    self.setStringBlock(
+                        x + num, y, char, color=color, bgcolor=self.DefBgColor)
                 else:
                     if shadow:
-                        self.setStringBlock(x, y+num+0.07, char, color=shadowcolor)
-                    self.setStringBlock(x, y+num, char, color=color)
+                        self.setStringBlock(
+                            x, y + num + 0.07, char, color=shadowcolor)
+                    self.setStringBlock(x, y + num, char, color=color)
 
     def textSimple(self, x, y, string, color=None):
         if color is None:
             color = self.DefColor
 
         for num, char in enumerate(string):
-            self.setString(x+num, y, char, color=color)
+            self.setString(x + num, y, char, color=color)
 
     def box(self, x, y, w, h, filled=1, color=None, bg=0):
         if color is None:
@@ -203,24 +244,44 @@ class asciify():
 
         if filled:
             if bg:
-                pygame.draw.rect(self.Background, color, (posX, posY, posW, posH))
+                pygame.draw.rect(self.Background, color,
+                                 (posX, posY, posW, posH))
             else:
-                pygame.draw.rect(self.Foreground, color, (posX, posY, posW, posH))
+                pygame.draw.rect(self.Foreground, color,
+                                 (posX, posY, posW, posH))
         else:
             if bg:
-                pygame.draw.rect(self.Background, color, (posX, posY, posW, self.CellY))
-                pygame.draw.rect(self.Background, color, (posX, posY, self.CellX, posH))
-                pygame.draw.rect(self.Background, color, (posX, posH+posY, posW+self.CellX, self.CellY))
-                pygame.draw.rect(self.Background, color, (posW+posX, posY, self.CellX, posH))
+                pygame.draw.rect(self.Background, color,
+                                 (posX, posY, posW, self.CellY))
+                pygame.draw.rect(self.Background, color,
+                                 (posX, posY, self.CellX, posH))
+                pygame.draw.rect(
+                    self.Background,
+                    color,
+                    (posX,
+                     posH + posY,
+                     posW + self.CellX,
+                     self.CellY))
+                pygame.draw.rect(self.Background, color,
+                                 (posW + posX, posY, self.CellX, posH))
             else:
-                pygame.draw.rect(self.Foreground, color, (posX, posY, posW, self.CellY))
-                pygame.draw.rect(self.Foreground, color, (posX, posY, self.CellX, posH))
-                pygame.draw.rect(self.Foreground, color, (posX, posH+posY, posW+self.CellX, self.CellY))
-                pygame.draw.rect(self.Foreground, color, (posW+posX, posY, self.CellX, posH))
+                pygame.draw.rect(self.Foreground, color,
+                                 (posX, posY, posW, self.CellY))
+                pygame.draw.rect(self.Foreground, color,
+                                 (posX, posY, self.CellX, posH))
+                pygame.draw.rect(
+                    self.Foreground,
+                    color,
+                    (posX,
+                     posH + posY,
+                     posW + self.CellX,
+                     self.CellY))
+                pygame.draw.rect(self.Foreground, color,
+                                 (posW + posX, posY, self.CellX, posH))
 
     def boxoutline(self, x, y, w, h, color=None, outline=None, bg=0):
         self.box(x, y, w, h, color=color, bg=bg)
-        self.box(x, y, w-1, h-1, color=outline, filled=0, bg=bg)
+        self.box(x, y, w - 1, h - 1, color=outline, filled=0, bg=bg)
 
     def terminate(self):
         pygame.quit()
@@ -276,4 +337,3 @@ class asciify():
                 self.tick(fps)
             if clear:
                 self.clear()
-
